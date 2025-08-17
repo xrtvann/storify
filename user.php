b61da5116333b5f7b1d421afa6c89e62817e0342
xrtvann <irvangoblas@gmail.com>
2025-08-17 20:02:05 +0700

    add: implement access control method in User class

--- user.php
+++ user.php
@@ -23,13 +23,26 @@ class User
         $str = "Welcome {$this->role} {$this->name}";
         return $str;
     }
+
+    public function getAccess() {
+        if ($this->role === 'Admin') {
+            return "Full access granted.";
+        } else {
+            return "Limited access granted.";
+        }
+    }
 }
 
 $user1 = new User(1, "John Doe", "johndoe", "password123", "Admin");
 $user2 = new User(2, "Jane Smith", "janesmith", "password456");
 echo $user1->getUserInfo();
 echo "<br>";
+echo $user1->getAccess();
+echo "<br>";
+
 echo $user2->getUserInfo();
+echo "<br>";
+echo $user2->getAccess();
 echo "<br>";
 
 ?>
