2f2b3c9c329526e5030154e6696d278730bec9b2
xrtvann <irvangoblas@gmail.com>
2025-08-18 11:49:08 +0700

    refactor: improve constructor formatting in Customer class

--- customer.php
+++ customer.php
@@ -1,7 +1,17 @@
 <?php
 
-class Customer extends User {
+class Customer extends User
+{
     public $role = 'customer';
+
+    public function __construct($id = null, $name = null, $username = null, $password = null, $role = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->username = $username;
+        $this->password = $password;
+        $this->role = $role;
+    }
 }
 
 ?>
