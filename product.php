f1560c581868e4a9bde15877e86707d4d80c7d03
xrtvann <irvangoblas@gmail.com>
2025-08-18 18:30:28 +0700

    add: include discount property and setDiscount method in Product class

--- product.php
+++ product.php
@@ -2,20 +2,27 @@
 
 class Product
 {
-    protected $id, $name, $price, $stock, $category;
+    protected $id, $name, $price, $stock, $category, $discount;
 
-    public function __construct($id = null, $name = null, $price = 0, $stock = 0, $category = null)
+    public function __construct($id = null, $name = null, $price = 0, $stock = 0, $category = null, $discount = 0)
     {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
         $this->stock = $stock;
         $this->category = $category;
+        $this->discount = $discount;
     }
 
     public function getProductInfo()
     {
         return "ID: $this->id, Name: $this->name, Price: $this->price, Stock: $this->stock, Category: $this->category";
+    }
+
+    public function setDiscount($discount)
+    {
+        $this->discount = $discount;
+        $this->price -= $this->price * ($discount / 100);
     }
 }
 
