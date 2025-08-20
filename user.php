9900fe426dd8d05bcac9910e466b12c2b8063e95
xrtvann <irvangoblas@gmail.com>
2025-08-20 19:14:37 +0700

    refactor: consolidate user property accessors and mutators in User class

--- user.php
+++ user.php
@@ -13,6 +13,54 @@ class User
         $this->role = $role;
     }
 
+    public function setName(string $name)
+    {
+        if (is_string($name) && trim($name) !== '') {
+            $this->name = $name;
+        }
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setUsername(string $username)
+    {
+        if (is_string($username) && trim($username) !== '') {
+            $this->username = $username;
+        }
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setPassword(string $password)
+    {
+        if (is_string($password) && trim($password) !== '') {
+            $this->password = password_hash($password, PASSWORD_DEFAULT);
+        }
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setRole(string $role)
+    {
+        if (in_array($role, ['customer', 'admin'])) {
+            $this->role = $role;
+        }
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
     public function getUserInfo()
     {
         $str = "Welcome {$this->role} {$this->name}";
